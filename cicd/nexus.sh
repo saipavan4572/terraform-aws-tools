a97dff15969a05db91423806728ac338e2fa8db2
saipavan4572 <saipavan4572@gmail.com>
2024-09-21 17:56:00 +0530

    adding nexus.sh file to install nexus

--- cicd/nexus.sh
+++ cicd/nexus.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo docker run hello-world
+
 docker run -d -p 8081:8081 sonatype/nexus3:latest
+## here we are using docker version of nexus image to install into the AWS instance becuase the nexus AMI instance subscription which was discussed in the session was not allowing to create the instance and failing with payment failed error message.
+## I have created this file to install nexus into the AWS instance while creating the instance by passing the commands to user data.
